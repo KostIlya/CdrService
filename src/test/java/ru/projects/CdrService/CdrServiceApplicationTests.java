0e1c07ac4c5f5e5004f4c0e3cb6b7fe6a5eda6a8
Ilya Kostylev <ilya.kostylev.1997@mail.ru>
2025-03-22 12:33:31 +0300

    Initial commit

---
+++ src/test/java/ru/projects/CdrService/CdrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.projects.CdrService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
